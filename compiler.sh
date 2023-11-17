401fa9fbdfcaf3c321423b70c2c32f8ecc001f71
Imam <134967655+imam1912@users.noreply.github.com>
2023-11-17 11:40:16 +0600

    0.1 version

--- compiler.sh
+++ compiler.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
-file="/data/data/com.termux/files/home/KotMux/condition.txt"
-try=$(cat $file)
-if(( try==0 ))
-then
-apt update && apt upgrade
-pkg install openjdk-17
-pkg install kotlin
-echo "1" > condition.txt
-else
-echo ">~<"
-fi
-GREEN='\033[0;36m'
-RED='\033[1;33m'
+#
+# Script For Compiling Kotlin Code Using Termux!
+# Before Continue Make Sure You Are On Root Directory
+# How to Check That?
+# Type pwd for checking the directory
+# Type cd ~ 
+# For Going To Root Directory
+#
+# by imam :)
+
+#Defining bash colour
+
+CYAN='\033[0;36m'
+YELLOW='\033[1;33m'
+
+#Getting file path for condition
+
+FILE="/data/data/com.termux/files/home/KotMux/condition.txt"
+
+#Reading the files value for applying the condition
+
+READ=$(cat $FILE)
+
+if(( READ==0 ))
+    then
+        apt update && apt upgrade
+        pkg install openjdk-17
+        pkg install kotlin
+        echo "1" > condition.txt
+    else
+        echo "All Packages Are Exsited"
+    fi
+
+#Spacing
+
 echo "                                      "
-echo -e "${RED}Which Project You Want to Compile?"
+
+#Hint for user input!
+
+echo -e "${YELLOW}Which Project You Want to Compile?"
+
 echo "                                      "
-read file
-filetwo="$file"
-mainFile="${filetwo%.*}"
+
+#Getting User Input of File Path
+
+read USER_INPUT
+USER_CON="$USER_INPUT"
+MAIN_PATH="${USER_CON%.*}"
+
+#Spcing
+
 echo "                                      "
 echo "Compiling..............."
 sleep 1s
-kotlinc $mainFile.kt -include-runtime -d $mainFile.jar
+
+#Conpiling Process
+
+kotlinc $MAIN_PATH.kt -include-runtime -d $MAIN_PATH.jar
+
+#Spacing
+
 echo "                                      "
-echo -e "${GREEN} >>>>>> Compiled Sucessfully!!<<<<<<${RED}"
+echo -e "${CYAN} >>>>>> Compiled Sucessfully!!<<<<<<${YELLOW}"
 echo "                                      "
-java -jar $mainFile.jar
+
+#Runing Jar File
+
+java -jar $MAIN_PATH.jar
+
 echo "                                      "
 sleep 1s
-rm $mainFile.jar
 
+#Remove Created Jar File
 
+rm $MAIN_PATH.jar
 
-
+#End
